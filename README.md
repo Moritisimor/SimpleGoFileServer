4adfa4c7b6d537e10143287a994d67f1bec8125d
Moritisimor <devMoritisimor@proton.me>
2025-09-18 21:55:09 +0200

    Changing the directory where files are served from is now possible. Also updated README.md and added .gitignore

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Simple Go File Server
-A very simple program written in go for hosting static files.
+A simple program written in go for hosting static files.
 
 ## How to compile
-To compile this program you will need a Go installation on your PC, preferably the newest one. Then, you need to clone the repository:
+To compile this program, you will need a Go installation on your PC, preferably the newest one. Then, you need to clone the repository:
 ```console
 git clone https://github.com/Moritisimor/SimpleGoFileServer
 ```
@@ -12,44 +12,61 @@ cd SimpleGoFileServer/SimpleGoFileServer
 ```
 And finally compile it with the Go compiler:
 ```console
-go build . 
+go build .
 ```
-Depending on your platform, you can then launch the Server by just double clicking the compiled binary file or typing the following in your terminal:
+Depending on your platform, you can then launch the Server by just double-clicking the compiled binary file 
+or typing the following in your terminal:
 ```console
 ./SimpleGoFileServer
 ```
 
 ## How to launch
-To start the server you either need the go file of the server along with go installed or the compiled binary file.
+To start the server, you either need the go file of the server along with go installed or the compiled binary file.
 
-By default, without any arguments given, the Server listens on all interfaces and on port 8000,
-so you can theoretically just double click the .exe file (or whatever format your OS uses) and it would run until the terminal window is closed, or the process is terminated in some other way.
+By default, without any arguments given, the Server listens on all interfaces on port 8000 and serves
+files from the "files" directory.
+
+It is possible to change the port, host and folder the server serves files from. The syntax is as follows:
 
 ### Basic syntax:
 ```
-SimpleGoFileServer <Port (Integer)> <Localhost (Boolean)>
+SimpleGoFileServer <Port (Integer)> <Localhost (Boolean)> <Directory (String/Text)>
 ```
 
 (Note: Boolean means true or false.)
 ## Examples
-### If you want to run the server on port 8000 and only on localhost, it would look like this:
+If you want to run the server on port 8000 and only on localhost, it would look like this:
 ```console
 SimpleGoFileServer 8000 true
 ```
 
-### If you want to run the server on port 7000 and listen on all interfaces it would look like this:
+If you want to run the server on port 7000 and listen on all interfaces, it would look like this:
 ```console
 SimpleGoFileServer 7000 false
 ```
 
+If you want to specify the directory where files are served from, as well, it would look like this:
+```console
+SimpleGoFileServer 7000 false exampleDirectory
+```
+File paths are relative to the directory where the server is launched from. Using absolute paths is not recommended.
+
 ## Error codes and their meanings
-#### 1: The selected port is not a number.
-#### 2: The selected port is not in the valid range of available ports.
-#### 3: 2nd Argument got a value which is not true or false.
-#### 4: Got more arguments than necessary.
+1: The selected port is not a number.
+
+2: The selected port is not in the valid range of available ports.
+
+3: The second Argument got a value which is not true or false.
+
+4: Got more arguments than necessary.
+
+5: The third Argument is empty or consists of only whitespaces.
+
+6: The Specified folder does not exist.
 
 ## How to use
-To make files available, you simply put them in the folder named "files" and they will be available there.
+To share files, you put them in the folder named "files" and they will be available there.
+Alternatively, you can also specify a different folder to serve files from. This option is the third argument.
 
 To access the files in the “files” directory, assuming the server runs on 192.168.1.10 on all interfaces, 
 you can either download them from your terminal with the cURL command like this:
@@ -62,5 +79,6 @@ Or visit it in your browser:
 http://192.168.1.10:8000/
 ```
 
-If a server listens on all interfaces that means it is available network-wide and maybe even over the internet if port-forwarding is active.
+If a server listens on all interfaces, that means it is available network-wide and maybe even over the internet if port-forwarding is active.
+
 If a server listens only on localhost, that means only your local machine can access it.
--- SimpleGoFileServer/Server.go
+++ SimpleGoFileServer/Server.go
@@ -8,22 +8,52 @@ import (
 	"strings"
 )
 
-// This function checks if a given string is worthy of being used as a port number, and if not, exits with corresponding exit codes.
-func isPortWorthy(checkee string) {
+// Check if a string is worthy of being a port number, and, if so, changes the original string to be that port number
+func changePort(checkee string, port *string) {
 	checkeeAsInt, err := strconv.ParseInt(checkee, 0, 32)
+	// Only parse to int to see if the string is a number at all and to check if it is in the valid range of numbers
 
 	if err != nil {
-		fmt.Println("Ports may only be numbers ranging from 1-65535")
+		fmt.Println("[ ERR ] Ports may only be numbers ranging from 1-65535")
 		os.Exit(1)
 	}
 
 	if checkeeAsInt < 1 {
-		fmt.Println("Selected port may not be smaller than 0.")
+		fmt.Println("[ ERR ] Selected port may not be smaller than 1.")
 		os.Exit(2)
 	} else if checkeeAsInt > 65535 {
-		fmt.Println("Selected port may not be bigger than 65535")
+		fmt.Println("[ ERR ] Selected port may not be bigger than 65535")
 		os.Exit(2)
 	}
+
+	*port = checkee
+}
+
+// Check if the 2nd argument is a boolean value, and, if so, change the original string accordingly.
+func changeHost(localhost string, host *string) {
+	if normalize(localhost) == "false" {
+		*host = "0.0.0.0:" // Listen on all interfaces
+	} else if normalize(localhost) == "true" {
+		*host = "localhost:"
+	} else {
+		fmt.Println("[ ERR ] 2nd Value may only be true or false.")
+		os.Exit(3)
+	}
+}
+
+func changeDir(checkee string, directory *string) {
+	if normalize(checkee) == "" {
+		fmt.Println("[ ERR ] Directory may not be empty.")
+		os.Exit(5)
+	}
+
+	_, err := os.Stat(checkee)
+	if err != nil {
+		fmt.Println("[ ERR ] The specified directory does not exist.")
+		os.Exit(6)
+	}
+
+	*directory = normalize(checkee)
 }
 
 func normalize(unnormalString string) string {
@@ -31,48 +61,58 @@ func normalize(unnormalString string) string {
 }
 
 func main() {
-	var port 			string
-	var host 			string
+	port := "8000"
+	host := "0.0.0.0:"
+	directory := "files"
 
 	switch len(os.Args) {
 	case 1:
-		fmt.Println("No args, standarding port to 8000 and listening on all interfaces.")
-		port = "8000"
-		host = "0.0.0.0:"
+		fmt.Println("[ INFO ] No args, standarding port to 8000, serving files in 'files' directory and " +
+			"listening on all interfaces.")
 
 	case 2:
-		isPortWorthy(os.Args[1])
-		port = os.Args[1]
-		host = "0.0.0.0:"
-		fmt.Printf("Port defined, listening on %s on all interfaces\n", port)
+		changePort(os.Args[1], &port)
+
+		fmt.Printf("[ INFO ] Port defined, serving files in 'files' directory and listening on "+
+			"port %s on all interfaces.\n", port)
 
 	case 3:
-		isPortWorthy(os.Args[1])
-		port = os.Args[1]
+		changePort(os.Args[1], &port)
+		changeHost(os.Args[2], &host)
 
-		if normalize(os.Args[2]) == "true" {
-			host = "localhost:"
-			fmt.Printf("Port and localhost defined, listening on %s on localhost.\n", port)
-		} else if normalize(os.Args[2]) == "false" {
-			host = "0.0.0.0:"
-			fmt.Printf("Port and localhost defined, listening on %s on all interfaces.\n", port)
+		fmt.Printf("[ INFO ] Port defined, serving files in 'files' directory and listening on port %s ", port)
+		if host == "0.0.0.0:" {
+			fmt.Println("on all interfaces.")
 		} else {
-			fmt.Println("Expected true or false value for 2nd argument.")
-			os.Exit(3)
+			fmt.Println("on localhost.")
 		}
 
 	case 4:
-		fmt.Println("Expected 0, 1 or 2 arguments, no more.")
+		changePort(os.Args[1], &port)
+		changeHost(os.Args[2], &host)
+		changeDir(os.Args[3], &directory)
+
+		fmt.Printf("[ INFO ] Port defined, serving files in '%s' directory and listening "+
+			"on port %s ", directory, port)
+		if host == "0.0.0.0:" {
+			fmt.Println("on all interfaces.")
+		} else {
+			fmt.Println("on localhost.")
+		}
+	}
+
+	if len(os.Args) > 4 {
+		fmt.Println("[ ERR ] Expected 0-3 Arguments, no more.")
 		os.Exit(4)
 	}
 
-	fmt.Println("Starting Server...")
-	fmt.Printf("Listening on Port %s.\n", port)
-	http.Handle("/", http.FileServer(http.Dir("files")))
-	fmt.Println("Files are available in the files directory.")
-	listeningErr := http.ListenAndServe(host + port, nil)
+	fmt.Println("[ INFO ] Starting Server...")
+	fmt.Printf("[ INFO ] Listening on Port %s.\n", port)
+	http.Handle("/", http.FileServer(http.Dir(directory)))
+	fmt.Printf("[ INFO ] Files are available in the %s directory.\n", directory)
+	listeningErr := http.ListenAndServe(host+port, nil)
 
 	if listeningErr != nil {
-		fmt.Printf("Something went wrong and the server will shut down now.\nError: %e\n", listeningErr)
+		fmt.Printf("[ ERR ] Something went wrong and the server will shut down now.\nError: %e\n", listeningErr)
 	}
 }
